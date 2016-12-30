cb07cf77f378875ea839dccc4b71c403c2b59d60
matthewcosgrove <mcosgrove@pivotal.io>
2016-12-30 15:34:09 +0000

    Improve naming convention of env var

--- 02-run-ansible-tasks.sh
+++ 02-run-ansible-tasks.sh
@@ -3,9 +3,9 @@
 set -eu
 
 : ${APPLE_EMAIL:?"Need to set APPLE_EMAIL non-empty"}
-vars_choice=${ANSIBLE_PROFILE:-main}
+vars_choice=${MACOS_BOOTSTRAP_PROFILE:-main}
 apple_email=${APPLE_EMAIL}
 echo "Running playbooks based on vars defined in vars/${vars_choice}.yml"
-echo "To override vars, set env var ANSIBLE_PROFILE to filename without suffix e.g. ANSIBLE_PROFILE=lite"
+echo "To override vars, set env var MACOS_BOOTSTRAP_PROFILE to filename without suffix e.g. MACOS_BOOTSTRAP_PROFILE=lite"
 ansible-galaxy install -r pre-req-roles.yml
 ansible-playbook --extra-vars "vars_choice=${vars_choice} apple_email=${apple_email}" main.yml -i inventory -K
