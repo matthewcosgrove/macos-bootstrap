c7139f77d26bdf287bedf5f1927abd469e93220f
Matthew Cosgrove <mcosgrove@pivotal.io>
2017-01-04 14:07:28 +0000

    Renaming to match Ansible Galaxy naming convention

--- 02-run-ansible-tasks.sh
+++ 02-run-ansible-tasks.sh
@@ -7,5 +7,5 @@ vars_choice=${MACOS_BOOTSTRAP_PROFILE:-main}
 apple_email=${APPLE_EMAIL}
 echo "Running playbooks based on vars defined in vars/${vars_choice}.yml"
 echo "To override vars, set env var MACOS_BOOTSTRAP_PROFILE to filename without suffix e.g. MACOS_BOOTSTRAP_PROFILE=lite"
-ansible-galaxy install -r pre-req-roles.yml
+ansible-galaxy install -r requirements.yml
 ansible-playbook --extra-vars "vars_choice=${vars_choice} apple_email=${apple_email}" main.yml -i inventory -K
