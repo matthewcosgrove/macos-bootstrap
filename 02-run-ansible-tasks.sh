33eeeaa1ba37219cd91c5ba4373115a0ca4c36bf
matthewcosgrove <mc.matthew.cosgrove@gmail.com>
2019-06-06 14:19:37 +0200

    Enable usage of latest bash. See https://apple.stackexchange.com/questions/224511/how-to-use-bash-as-default-shell

--- 02-run-ansible-tasks.sh
+++ 02-run-ansible-tasks.sh
@@ -8,4 +8,5 @@ apple_email=${APPLE_EMAIL}
 echo "Running playbooks based on vars defined in vars/${vars_choice}.yml"
 echo "To override vars, set env var MACOS_BOOTSTRAP_PROFILE to filename without suffix e.g. MACOS_BOOTSTRAP_PROFILE=lite"
 ansible-galaxy install -r requirements.yml
-ansible-playbook --extra-vars "vars_choice=${vars_choice} apple_email=${apple_email}" main.yml -i inventory -K
+ansible-playbook --extra-vars "vars_choice=${vars_choice} apple_email=${apple_email}" main.yml -i inventory -K --ask-become-pass
+#ansible-playbook --extra-vars "vars_choice=${vars_choice}" main.yml -i inventory -K --ask-become-pass
